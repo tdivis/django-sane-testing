a1e51198f62768ec993faee21b7bd170ce9adb08
Tomas Divis <tomas.divis@nic.cz>
2013-02-14 15:23:54 +0100

    Updated test projects settings and urls to Django 1.4.

--- testproject/settings.py
+++ testproject/settings.py
@@ -8,9 +8,9 @@ APPLICATION_ROOT = path.join(path.dirname(path.abspath(__file__)))
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-    'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader'
 )
 
 # AUTHENTICATION_BACKENDS = ('keykeeper.libopenid.OpenidBackend',)
@@ -33,7 +33,7 @@ TEMPLATE_DIRS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.media",
     "django.core.context_processors.request",
 )
